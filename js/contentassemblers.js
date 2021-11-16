9b798b62912557e03bc4d487b71cde85abc1d2dd
sky406 <60670435+sky406@users.noreply.github.com>
2021-11-16 13:23:59 -0800

    contentassembler script and variables added
    the function still doesn't  work at the moment but it's better i save now

---
+++ js/contentassemblers.js
@@ -0,0 +1,17 @@
+// the variable site info builds all the info for the site info blocks
+/* the format for siteinfo is 
+[[title,description],[image link,alt],[true,button text]] 
+true iplies that the block ahs a button underr the text*/
+var siteinfo = [
+    [["About us","[insert text here]"],[null,"a cool picture"],[true,"Come dine with us"]],
+    [["Our food","[insert text here]"],[null,"some cool seafood"],[true,"Check out our menu"]],
+    [["Our star chefs","[insert text here]"],[null,"a chef i guess"],[false,null]]
+    // TODO actually insert the proper info later
+    ]
+
+var imagelist = []
+    //TODO add a bunch of image links to make this work  
+function assembler(id,rows,type)
+{
+
+}
