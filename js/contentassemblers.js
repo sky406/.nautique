723d7576990339f0a9c3342402a703fe41107bfc
sky406 <60670435+sky406@users.noreply.github.com>
2021-11-25 23:29:17 -0800

    fixed maincontent function

--- js/contentassemblers.js
+++ js/contentassemblers.js
@@ -35,74 +35,110 @@ function maincontent(id,rows)
     {
         console.log('it tried')
         content +="<div class='row'>"
-        if(flip == false)
-        {
-            // text content
-            console.log(rows[i][0][1]) //remove later
-            content+= "<div class='box col-6'>"
-            content+= "<h2>" + rows[i][0][0] + "</h2>"
-            content+= "<p>" + rows[i][0][1] + "</p> "
-
-            if(rows[i][2][0] == true)
+        Text = ''
+            //txt
+            Text +="<div class='box col-6'>"
+            Text +="<div class='container txt-box'>"
+            Text +="<div class='row justify-content-center'><h2 class='txt txt-normal'>"+rows[i][0][0]+"</h2></div>"
+            Text +="<div class='row justify-content-center'><p class='txt txt-normal'>"+rows[i][0][1]+"</p></div>"
+            if (rows[i][2][0] == true)
             {
-                content+="<a href='" +rows[i][2][2]+"'><button class='cta_button'><span>" + rows[i][2][1] + "</span></button></a></div>"
+                Text += "<div class='row justify-content-center'><a href='"+rows[1][2][2]+"'class='cta_link'><button class='cta_button'><span class'txt cta_button_txt'>"+ rows[i][2][1] + "</span></button></a></div>"
+                Text += "</div></div>"
             }
             else
             {
-                content+='</div'
+                Text += "</div></div>"
             }
-            
-            // image content
+        img = ''
+            // img
             if(rows[i][1][0] == null)
             {
-                content+= add_placeholders(1,'box col-6','image'+i)
-                content+="</div>"
+                img += add_placeholders(1,'box col-6','image'+i)
             }
             else
             {
-                // altewrnate idea would be to make the image the background
-                content+="<div class='box col-6' style='background-image:"+ rows[i][1][0]+";background-position: center; background-repeat: no-repeat; background-size: cover;'></div> "
-                content+="</div>"    
+                img += "<div class='box col-6' style='background-image:"+ rows[i][1][0]+";background-position: center; background-repeat: no-repeat; background-size: cover;'></div>"
             }
-            // this is a pain in the ass
+        if(flip == false)
+        {
+            content+= Text + img
             flip = true
         }
         else
         {
-            // image content
-            if(rows[i][1][0] == null)
-            {
-                content+= add_placeholders(1,'box col-6','image'+i)
-            }
-            else
-            {
-                // altewrnate idea would be to make the image the background
-                content+="<div class='box col-6' style='background-image:"+ rows[i][1][0]+";background-position: center; background-repeat: no-repeat; background-size: cover;'></div> "    
-            }
-            
-            // text content
-            console.log(rows[i][0][1]) //remove later
-            content+= "<div class='box col-6'>"
-            content+= "<h2>" + rows[i][0][0] + "</h2>"
-            content+= "<p>" + rows[i][0][1] + "</p> "
-
-            if(rows[i][2][0] == true)
-            {
-                content+="<a href='" +rows[i][2][2]+"'><button class='cta_button'><span>" + rows[i][2][1] + "</span></button></a></div></div>"
-            }
-            else
-            {
-                content+='</div></div>'
-            }
-            
-            
-            // this is a pain in the ass
+            content+= img + Text
             flip = false
         }
+
+        // if(flip == false)
+        // {
+        //     // text content
+        //     console.log(rows[i][0][1]) //remove later
+        //     content+= "<div class='box col-6'>"
+        //     content+= "<h2>" + rows[i][0][0] + "</h2>"
+        //     content+= "<p>" + rows[i][0][1] + "</p> "
+
+        //     if(rows[i][2][0] == true)
+        //     {
+        //         content+="<a href='" +rows[i][2][2]+"'><button class='cta_button'><span>" + rows[i][2][1] + "</span></button></a></div>"
+        //     }
+        //     else
+        //     {
+        //         content+='</div'
+        //     }
+            
+        //     // image content
+        //     if(rows[i][1][0] == null)
+        //     {
+        //         content+= add_placeholders(1,'box col-6','image'+i)
+        //         content+="</div>"
+        //     }
+        //     else
+        //     {
+        //         // altewrnate idea would be to make the image the background
+        //         content+="<div class='box col-6' style='background-image:"+ rows[i][1][0]+";background-position: center; background-repeat: no-repeat; background-size: cover;'></div> "
+        //         content+="</div>"    
+        //     }
+        //     // this is a pain in the ass
+        //     flip = true
+        // }
+        // else
+        // {
+        //     // image content
+        //     if(rows[i][1][0] == null)
+        //     {
+        //         content+= add_placeholders(1,'box col-6','image'+i)
+        //     }
+        //     else
+        //     {
+        //         // altewrnate idea would be to make the image the background
+        //         content+="<div class='box col-6' style='background-image:"+ rows[i][1][0]+";background-position: center; background-repeat: no-repeat; background-size: cover;'></div> "    
+        //     }
+            
+        //     // text content
+        //     console.log(rows[i][0][1]) //remove later
+        //     content+= "<div class='box col-6'>"
+        //     content+= "<h2>" + rows[i][0][0] + "</h2>"
+        //     content+= "<p>" + rows[i][0][1] + "</p> "
+
+        //     if(rows[i][2][0] == true)
+        //     {
+        //         content+="<a href='" +rows[i][2][2]+"'><button class='cta_button'><span>" + rows[i][2][1] + "</span></button></a></div></div>"
+        //     }
+        //     else
+        //     {
+        //         content+='</div></div>'
+        //     }
+            
+            
+        //     // this is a pain in the ass
+        //     flip = false
+        // }
     }
     document.getElementById(id).innerHTML=content
 }
-// maincontent('main',siteinfo)
+maincontent('main',siteinfo)
 
 function gallerycontent(id,gallery)
 {
